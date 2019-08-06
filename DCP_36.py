d7c68bf8cb715214eada1b6a138aa6d07b7cf0da
Jeremi Do Dinh <43282168+jdodinh@users.noreply.github.com>
2019-08-06 12:15:48 -0400

    Commented

--- DCP_36.py
+++ DCP_36.py
@@ -3,15 +3,15 @@ Given the root to a binary search tree, find the second largest node in the tree
 """
 
 
-class Node:
-    def __init__(self, right, left, parent, val: int):
+class Node:                                                     # Creating a Node object, includes left, right and parent pointers
+    def __init__(self, right, left, parent, val: int):          # Parent pointers are not necessary in this version of implementation
         self.right = right
         self.left = left
         self.parent = parent
         self.val = val
 
 
-def init_tree():
+def init_tree():                                                # Initializing an arbitrary BST
     root = Node(right=None, left=None, parent=None,  val=5)
     one = Node(right=None, left=None, parent=None,  val=1)
     two = Node(right=None, left=None, parent=None,  val=2)
@@ -44,24 +44,24 @@ def init_tree():
     return root
 
 
-def find_2nd_smallest(root: Node):
-    v = root
-    while v.right.right is not None:
+def find_2nd_smallest(root: Node):                              # Function that find the second largest, given root of the tree
+    v = root                                                    # We start off by finding the rightmost node in the tree, thus the largest node.
+    while v.right.right is not None:                            # The second largest node is the largest node in its left subtree. 
         v = v.right
-    if v.right.left is None:
-        return v
-    if v.right.left.right is None:
-        return v.right.left
-    v = v.right.left
+    if v.right.left is None:                                    # At this point v.right is the largest node in the tree. If it doesn't have a left 
+        return v                                                # child, the second largest node is v (v.right's parent)
+    if v.right.left.right is None:                              # If the largest node's left child doesnt have a right child, the second largest 
+        return v.right.left                                     # node is that left child
+    v = v.right.left                                            # else we find the largest node in the left subtree of the largest node in the tree
     while v.right is not None:
         v = v.right
-    return v.val
+    return v.val                                                # return the value
 
 
 def main():
-    root = init_tree()
-    val = find_2nd_smallest(root)
-    print(val)
+    root = init_tree()                                          # Initialize tree
+    val = find_2nd_smallest(root)                               # Find second largest node
+    print(val)                                                  # Print Value
 
 
 if __name__ == "__main__":
